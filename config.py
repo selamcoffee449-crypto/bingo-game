c7cf8640e7336846f2c2f3e4109cb3e4db2718ad
selamcoffee449-crypto <selamcoffee449@gmail.com>
2026-02-15 18:08:19 +0300

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
 
-# Telegram Bot Token (set in Railway Variables)
-TOKEN = os.getenv("8496790065:AAHc1nCLXu6MyhzIce_B_aylJzq_UOyoiWI")
+# Telegram Bot Token (comes from Railway variables)
+TOKEN = os.getenv("BOT_TOKEN")
 
-# Admin IDs (comma separated numbers in Railway)
-# example: 123,456,789
-admin_raw = os.getenv("6835994100", "")
+if not TOKEN:
+    raise ValueError("BOT_TOKEN is missing!")
+
+# Admin IDs
+admin_raw = os.getenv("ADMIN_IDS", "")
 ADMIN_IDS = [int(x) for x in admin_raw.split(",") if x.strip()]
 
-# Game settings (you can still override from Railway if you want)
+# Game settings
 TICKET_PRICE = int(os.getenv("TICKET_PRICE", 10))
 ROUND_TIME = int(os.getenv("ROUND_TIME", 60))
